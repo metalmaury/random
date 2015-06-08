70fa94b53d8e515eeddad539a6ad53f3c43e77da
mmromero <mauricio_martinez_romero@yahoo.es>
2015-06-08 15:54:54 +0200

    version108062015

---
+++ random.js
@@ -0,0 +1,8 @@
+var numero = Math.ramdom();
+
+if(numero <= 0.5){
+  consoloe.log('\n' +numero + ' menor que 0,5 \n');
+}else{
+  consoloe.log('\n' +numero + ' mayor que 0,5 \n');
+}
+console.log('fin');
---
+++ text.js
@@ -0,0 +1,8 @@
+var text = {
+  1:'zariguella',
+  2:'melones',
+  3:'sandias',
+  4:'acelgas',
+  5:'espinacas'
+}
+console.log(txt.1());
